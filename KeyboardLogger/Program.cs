cd6bb3db74de1540e60ac93f99986292c883208b
William Taylor <wi11berto@yahoo.co.uk>
2017-01-22 21:31:06 +0000

    adding logger and number hack

---
+++ KeyboardLogger/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using Keystroke.API;
+
+namespace KeyboardLogger
+{
+    class Program
+    {
+        private static List<string> keys = new List<string>();
+
+        static void Main(string[] args)
+        {
+            using (var api = new KeystrokeAPI())
+            {
+                api.CreateKeyboardHook((character) =>
+                {
+                    if (character.KeyCode == KeyCode.Escape)
+                    {
+                        Application.Exit();
+                    }
+                    else if (character.KeyCode == KeyCode.Tab)
+                    {
+                        var total = string.Empty;
+
+                        foreach (var key in keys)
+                        {
+                            total += key;
+                        }
+                        
+                        MessageBox.Show(total, "Keys Pressed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        keys.Clear();
+                    }
+                    else
+                    {
+                        keys.Add(character.KeyCode.ToString());
+                    }
+                });
+
+                Application.Run();
+            }
+        }
+    }
+}
---
+++ NumberHack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NumberHack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter a number between: {int.MinValue} -> {int.MaxValue} ");
+
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = int.MinValue; i < int.MaxValue; i++)
+            {
+                if (number == i)
+                {
+                    Console.WriteLine($"Your number was {i} ... Use strong passwords!");
+                } 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
